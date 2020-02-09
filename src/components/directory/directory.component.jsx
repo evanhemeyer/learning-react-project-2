492638372e8ba72f49c2e0a96fe176910c457831
evanhemeyer <hemeyer.evan@gmail.com>
2020-02-08 19:41:37 -0600

    update menu-items to navigate withRouther HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { withRouter } from "react-router-dom";
+
 import MenuItem from "../menu-item/menu-item.component.jsx";
 
 import "./directory.styles.scss";
@@ -50,8 +52,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
